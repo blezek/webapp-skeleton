1f2a19cb36f3a127c08a2bc3a16aef1cdd49f899
Daniel Blezek <daniel.blezek@gmail.com>
2014-09-15 07:34:04 -0500

    Beginnings of an XTK display

--- src/main/javascript/mrb/index.js
+++ src/main/javascript/mrb/index.js
@@ -1,6 +1,25 @@
 var JSZip = require ( 'jszip' );
+var utils = require ('../utils');
 
+module.exports.MRB = function ( buffer ) {
+  this.zip = new JSZip(buffer);
 
+  // Get the models from the MRB file
+  this.getModels = function () {
+    var models = [];
+    console.log(this.zip);
+    for ( var key in this.zip.files) {
+      console.log("Key: " + key);
+      if (utils.endsWith(key,".vtk")) {
+        models.push(key);
+      }
+    }
+    return models;
+  };
+
+};
+
+/** This is the readMRB function */
 module.exports.readMRB = function ( buffer ) {
   var zip = new JSZip(buffer);
   return zip;
