f4267285db7ef0d0db70fcc45fa4dbfaa0dab84a
Daniel Blezek <daniel.blezek@gmail.com>
2015-04-24 12:26:17 -0500

    Cleaned up Gulp

--- gulp/tasks/html.js
+++ gulp/tasks/html.js
@@ -3,56 +3,59 @@ var gulp = require ('gulp');
 
 gulp.task ( 'html', ['css'], function() {
   gulp.src('src/app/assets/html/**')
-  .pipe ( gulp.dest( './build/') );
+    .pipe ( gulp.dest( './build/') );
 
   gulp.src('src/app/assets/js/**')
-  .pipe ( gulp.dest( './build/js/') );
+    .pipe ( gulp.dest( './build/js/') );
 
   gulp.src('src/app/assets/images/**')
-  .pipe ( gulp.dest( './build/images/') );
+    .pipe ( gulp.dest( './build/images/') );
 
   gulp.src('src/test/assets/**')
-  .pipe ( gulp.dest('./build/data/'));
+    .pipe ( gulp.dest('./build/data/'));
 
   gulp.src(['node_modules/dat-gui/vendor/dat*.js'])
-  .pipe ( gulp.dest('./build/js/'));
+    .pipe ( gulp.dest('./build/js/'));
 
 
   // XTK Local build (if available)
   // should copy for production
   gulp.src(['../X/utils/xtk.js'])
-  .pipe ( gulp.dest('./build/js'));
+    .pipe ( gulp.dest('./build/js'));
 
   // "Vendor" packages
   // Bootstrap
   gulp.src(['node_modules/bootstrap/dist/**'])
-  .pipe ( gulp.dest('./build'));
+    .pipe ( gulp.dest('./build'));
 
   // Font Awesome
   gulp.src(['node_modules/font-awesome/fonts/**'])
-  .pipe( gulp.dest('./build/fonts'));
+    .pipe( gulp.dest('./build/fonts'));
   gulp.src(['node_modules/font-awesome/css/**'])
-  .pipe( gulp.dest('./build/css'));
+    .pipe( gulp.dest('./build/css'));
 
   // JQuery
   gulp.src(['node_modules/jquery/dist/**'])
-  .pipe( gulp.dest('./build/js'));
+    .pipe( gulp.dest('./build/js'));
 
   // Progressbar.js
   gulp.src(['node_modules/progressbar.js/dist/*js'])
-  .pipe(gulp.dest('./build/js'));
-
+    .pipe(gulp.dest('./build/js'));
 
   // dat.gui
   gulp.src(['node_modules/dat-gui/vendor/dat.gui.js'])
-  .pipe(gulp.dest('./build/js'));
+    .pipe(gulp.dest('./build/js'));
   
+  // knockout
+  gulp.src(['node_modules/knockout/build/output/knockout-latest.debug.js'])
+    .pipe(gulp.dest('./build/js/'));
 
+  
   // Dropzone file upload
   gulp.src(['node_modules/dropzone/downloads/*js'])
-  .pipe(gulp.dest('./build/js'));
+    .pipe(gulp.dest('./build/js'));
   gulp.src(['node_modules/dropzone/downloads/css/**'])
-  .pipe(gulp.dest('./build/css'));
+    .pipe(gulp.dest('./build/css'));
   gulp.src(['node_modules/dropzone/downloads/images/**'])
-  .pipe(gulp.dest('./build/images'));
+    .pipe(gulp.dest('./build/images'));
 });
